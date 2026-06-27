49cf3ebc915e50734455779ab1100e9f7aa29da2
SASAKI Hideo <hideosasaki@users.noreply.github.com>
2026-06-28 01:43:33 +0900

    サンプルとして揺さぶりのイベントを追加

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 cubeGrip.onChange(function (strength) {
     cubeVibe.start(strength)
 })
+input.onGesture(Gesture.Shake, function () {
+    music.setVolume(10)
+    music.play(music.createSoundExpression(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
 cubeTouch.onPinReleased(function (face) {
     cubeLight.setBrightness(0)
 })
