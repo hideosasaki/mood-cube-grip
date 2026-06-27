4a66da88efa4492082fb63a9625ac527daa96af7
SASAKI Hideo <hideosasaki@users.noreply.github.com>
2026-06-28 01:20:50 +0900

    サンプルを実装

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+cubeGrip.onChange(function (strength) {
+    cubeVibe.start(strength)
 })
+cubeTouch.onPinReleased(function (face) {
+    cubeLight.setBrightness(0)
+})
+cubeTouch.onPinStuck(function (face) {
+    cubeLight.setColor(neopixel.colors(NeoPixelColors.Red))
+    cubeLight.setBrightness(9)
+})
+cubePair.setRole(CubeRole.Grip)
