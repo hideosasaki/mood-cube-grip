e6be557bad9f266c5dad7923ad1a1028dd7da6b1
SASAKI Hideo <hideosasaki@users.noreply.github.com>
2026-07-12 19:57:21 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,4 @@ input.onGesture(Gesture.Shake, function () {
     music.setVolume(10)
     music.play(music.createSoundExpression(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 })
-cubeTouch.onPinReleased(function (face) {
-    cubeLight.setBrightness(0)
-})
-cubeTouch.onPinStuck(function (face) {
-    cubeLight.setColor(neopixel.colors(NeoPixelColors.Red))
-    cubeLight.setBrightness(9)
-})
 cubePair.setRole(CubeRole.Grip)
