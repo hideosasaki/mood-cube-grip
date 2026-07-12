21c1854d9e4bf29e23abe17e01c7d8b3e01249cc
SASAKI Hideo <hideosasaki@users.noreply.github.com>
2026-07-12 19:56:02 +0900

    復元 '2026/7/7 13:09:51 mood-cube-blocks拡張を最新版 (電源投入時のバイブ空転・誤検出修正) に更新'

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
+cubeGrip.onChange(function (strength) {
+    cubeVibe.start(strength)
+})
 input.onGesture(Gesture.Shake, function () {
     music.setVolume(10)
     music.play(music.createSoundExpression(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 })
+cubeTouch.onPinReleased(function (face) {
+    cubeLight.setBrightness(0)
+})
 cubeTouch.onPinStuck(function (face) {
     cubeLight.setColor(neopixel.colors(NeoPixelColors.Red))
     cubeLight.setBrightness(9)
-    cubeVibe.pulse(5, 200)
-    cubeLight.setBrightness(0)
 })
 cubePair.setRole(CubeRole.Grip)
