972abd8a1e8be20f2f6b937c264529ed5258b372
SASAKI Hideo <hideosasaki@users.noreply.github.com>
2026-07-17 00:33:21 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 cubeGrip.onChange(function (strength) {
-    cubeVibe.start(strength)
+    cubeLight.setColor(neopixel.colors(NeoPixelColors.Blue))
+    cubeLight.setBrightness(strength)
 })
 input.onGesture(Gesture.Shake, function () {
-    music.setVolume(10)
+    cubeVibe.start(2)
     music.play(music.createSoundExpression(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    cubeVibe.stop()
 })
 cubePair.setRole(CubeRole.Grip)
+music.setVolume(90)
