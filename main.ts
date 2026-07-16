1da8070a6da73a41d5189b05c7be6b3f35753ab9
SASAKI Hideo <hideosasaki@users.noreply.github.com>
2026-07-17 00:39:41 +0900

    音を変える

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ cubeGrip.onChange(function (strength) {
 })
 input.onGesture(Gesture.Shake, function () {
     cubeVibe.start(2)
-    music.play(music.createSoundExpression(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
     cubeVibe.stop()
 })
 cubePair.setRole(CubeRole.Grip)
