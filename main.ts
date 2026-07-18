d0f06637a2fd27164df94f48fd98f219579330bc
SASAKI Hideo <hideosasaki@users.noreply.github.com>
2026-07-19 08:11:14 +0900

    bump to 1.1.3

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 cubeGrip.onMaxReleased(function () {
     music.setVolume(255)
-    music.play(music.createSoundExpression(WaveShape.Sine, 1600, 399, 255, 255, 1000, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(WaveShape.Sine, 5000, 416, 126, 251, 1000, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
     music.setVolume(90)
 })
 cubeGrip.onChange(function (strength) {
@@ -9,18 +9,26 @@ cubeGrip.onChange(function (strength) {
 })
 input.onGesture(Gesture.Shake, function () {
     cubeVibe.start(2)
-    music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+    if (音 == 1) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+    } else {
+        music.play(music.stringPlayable("C D E F G A B C5 ", 1000), music.PlaybackMode.UntilDone)
+    }
     cubeVibe.stop()
 })
 cubeTouch.onPinStuck(function (face) {
+    音 = 0
     cubeLight.setColor(neopixel.colors(NeoPixelColors.Green))
     cubeLight.setBrightness(1)
-    basic.pause(100)
+    basic.pause(5000)
     cubeLight.setBrightness(0)
     cubeLight.setColor(neopixel.colors(NeoPixelColors.Red))
+    音 = 1
 })
 cubeGrip.onMaxReached(function () {
     cubeVibe.pulse(5, 5000)
 })
+let 音 = 0
 cubePair.setRole(CubeRole.Grip)
 music.setVolume(90)
+音 = 1
