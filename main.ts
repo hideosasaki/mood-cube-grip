6bde585a70144e265ffc4f9cf655ce982ec0a7b7
SASAKI Hideo <hideosasaki@users.noreply.github.com>
2026-08-30 17:31:59 +0900

    bump to 1.1.4

--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
-cubeGrip.onMaxReleased(function () {
-    music.setVolume(255)
-    music.play(music.createSoundExpression(WaveShape.Sine, 5000, 416, 126, 251, 1000, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
-    music.setVolume(90)
-})
 cubeGrip.onChange(function (strength) {
     cubeLight.setColor(neopixel.colors(NeoPixelColors.Red))
     cubeLight.setBrightness(strength)
 })
 input.onGesture(Gesture.Shake, function () {
-    cubeVibe.start(2)
     if (音 == 1) {
         music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
     } else {
@@ -18,17 +12,37 @@ input.onGesture(Gesture.Shake, function () {
 })
 cubeTouch.onPinStuck(function (face) {
     音 = 0
+    cubeLight.setBrightness(3)
+    cubeLight.setColor(neopixel.colors(NeoPixelColors.Orange))
+    basic.pause(100)
+    cubeLight.setColor(neopixel.colors(NeoPixelColors.Yellow))
+    basic.pause(100)
     cubeLight.setColor(neopixel.colors(NeoPixelColors.Green))
-    cubeLight.setBrightness(1)
-    basic.pause(5000)
-    cubeLight.setBrightness(0)
-    cubeLight.setColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(100)
+    cubeLight.setColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(100)
+    cubeLight.setColor(neopixel.colors(NeoPixelColors.Indigo))
+    basic.pause(100)
+    cubeLight.setColor(neopixel.colors(NeoPixelColors.Violet))
+    basic.pause(100)
+    cubeLight.setColor(neopixel.colors(NeoPixelColors.Purple))
+    basic.pause(100)
+    cubeLight.setColor(neopixel.colors(NeoPixelColors.White))
+    basic.pause(100)
+    cubeLight.setColor(neopixel.colors(NeoPixelColors.Black))
     音 = 1
 })
 cubeGrip.onMaxReached(function () {
-    cubeVibe.pulse(5, 5000)
+    music.play(music.createSoundExpression(WaveShape.Square, 307, 20, 183, 255, 200, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    cubeVibe.pulse(1, 100)
+    music.play(music.createSoundExpression(WaveShape.Square, 307, 20, 183, 255, 100, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    cubeVibe.pulse(2, 100)
+    music.play(music.createSoundExpression(WaveShape.Square, 307, 20, 183, 255, 200, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    cubeVibe.pulse(3, 100)
+    music.play(music.createSoundExpression(WaveShape.Square, 307, 20, 183, 255, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    cubeVibe.pulse(5, 500)
 })
 let 音 = 0
 cubePair.setRole(CubeRole.Grip)
-music.setVolume(90)
+music.setVolume(255)
 音 = 1
